a16c36ae02fb7c4b5eef5932f7a306f4c98e88f5
anamika1803 <98591661+anamika1803@users.noreply.github.com>
2022-09-23 19:51:26 +0530

    positive non positive

---
+++ positive or non-positive.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("Enter the value in n\n");
+	scanf("%d",&n);
+	if(n>10)
+	{
+		printf("The no. is positive");
+	}
+else
+	{
+		printf("The no. is non-positive");
+	}
+	getch();
+}
