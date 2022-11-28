f4f934272f2615e380af7242c0dea7ced74287cb
anamika1803 <98591661+anamika1803@users.noreply.github.com>
2022-11-28 16:38:40 +0530

    vowels and consonents

---
+++ vowel and consonant.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c;
+	bool islowercasevowel,isuppercasevowel;
+	cout<<"Enter an alphabet:";
+	cin>>c;
+for(c=0;c>=1;c++){
+}
+	if(islowercasevowel>isuppercasevowel)
+	cout<<c<<"is a vowel.";
+	else
+	cout<<c<<"is a consonant.";
+	return 0;
+}
